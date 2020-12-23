274d2b63b89ab60a8763cdbab403a3338d7304e5
unknownpgr <unknownpgr@gmail.com>
2020-12-23 09:33:51 +0000

    Minor update

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => res.redirect("/index.html"));
 app.post("/upload", upload.single("img"), (req, res) => {
+  console.log(new Date());
   console.log(req.file);
+  cosnole.log('');
   res.status(200).send({ filename: path.join("imgs", req.file.filename) });
 });
 
-app.listen(4998, () => console.log("Server started"));
+app.listen(1502, () => console.log("Server started"));
